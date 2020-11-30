a55d2ee3a742bb1fc0631630c3c9046cd4dbf10a
gentashandi <genta.shandi@work.bri.co.id>
2020-11-30 10:43:01 +0700

    Add homework

--- task2.js
+++ task2.js
@@ -155,3 +155,81 @@ console.log("Jawaban 4:\n", namrud)
   ],
 }
 */
+
+const videoData = [
+  {
+    name: 'Miss Scarlet',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
+  },
+  {
+    name: 'Mrs. White',
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
+  },
+  {
+    name: 'Reverend Green',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
+  },
+  {
+    name: 'Rusty',
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
+  },
+  {
+    name: 'Colonel Mustard',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
+  },
+  {
+    name: 'Professor Plum',
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { 'dining room': false },
+      { 'billiard room': false },
+      { library: false },
+    ],
+  },
+];
+
+let presents = videoData.filter((present) => present.present === true);
+console.log("Jawaban Homework:", presents.length,"\n",presents)
